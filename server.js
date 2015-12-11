dc434b0f0e5d4cc2721b0c51c5121dc9c02c85f2
Jonkrone <jkrone@vt.edu>
2015-12-10 23:19:47 -0700

    Awesome! Changed our file server to use jQuery and AJAX for the most part. I still need to change the project list request but it's late and I feel good to pause at this point. Ciao!

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
+var fs = require('fs'), path = require('path');
+
 var express = require('express');
 var app = express();
+var PROJECT_PATH = "public/projects/";
 
 // Setting port property. env.PORT is online port while 5555 is default localhost
 app.set('port', (process.env.PORT || 5555));
@@ -20,18 +23,21 @@ app.get('/', function(request, response) {
 
 // GET requests to projectlist return a string that represents all of the files in public/projects
 app.get('/projectlist', function(request, response) {
-	var fileList = findFiles("public/projects/");
-
+	var fileList = findProjectFiles();
+	fileList.forEach(function(filepath) {
+		filepath = filepath.replace('public/projects/', "");
+	});
 	response.send(fileList);
 });
 
 // This route is used to return the full HTML document at the URI described by the query. Ugly query access I'm sure.
 // TODO:: remove public/projects from query (embed into findFiles)
 app.get('/projects', function(request, response) {
-	var projectLink = decodeURIComponent(request.url);
-	projectLink = projectLink.substring(projectLink.indexOf("?") + 1);
 
-	response.sendFile(projectLink, {root: __dirname});
+	var projectLink = decodeURIComponent(request.query.remotePath);
+	var project = fs.readFileSync(projectLink, 'utf8');
+
+	response.status(200).json({htmlSource: project, javascriptSource: null});
 });
 
 // Beep boop boop beep
@@ -40,13 +46,16 @@ app.listen(app.get('port'), function() {
 });
 
 
-var fs = require('fs'), path = require('path');
+
 var projectFiles = []; // I know I shouldn't be defining a variable outside of a container because 
 // it reduces independence and its scope is too high.
 
 // to create a directory tree, we can iterate through storing all relevent files in a tree and then
 // compute the tree into a selection list
-function findFiles(pathRoot) {
+function findProjectFiles(pathRoot) {
+	if (pathRoot === undefined) {
+		pathRoot = "public/projects/";
+	}
 
 	if (!fs.existsSync(pathRoot))
 		throw new Error("Attempting to dive into a path that does not exist" + pathRoot);
@@ -58,7 +67,7 @@ function findFiles(pathRoot) {
 		
 		if(stat.isDirectory())
 			// If directory, insert unclickable option in select list declaring the file dir
-			findFiles(filename);
+			findProjectFiles(filename);
 		else {
 			// console.log("filename:", filename);
 			if (projectFiles.indexOf(filename) == -1) projectFiles.push(filename);
